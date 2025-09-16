ffc5aafaae061516652315e6abc0f37ef9420b0e
aparnaproshell <aparna.harry87@student.qcc.cuny.edu>
2025-09-16 19:29:53 +0000

    lab 6 assignment completed exercise 1 and exercise 2

--- lab6/lab6_Harry.cpp
+++ lab6/lab6_Harry.cpp
@@ -26,10 +26,13 @@ int main()
     else if (n < 0)
     {
         if (n % 2 == 0)
+        {
             cout << "The number is NEGATIVE and EVEN" << endl;
-
+        }
         else
+        {
             cout << "The number is NEGATIVE and ODD" << endl;
+        }
     }
     else
     {
@@ -132,41 +135,93 @@ int main()
 
     cout << "\n ----- EXERCISE -----" << endl;
 
-//create a program to satisfy the the affordability price to a type of home
+
+
+    //create a program to satisfy the affordability price to a type of home
+    // declare integer variable to hold savings amount
     int afford;
-        cout<<"Enter the amount of money on saving, "<<endl;
-        cin>>afford;
-    if (afford<199999 && afford>0)
+    cout << "Enter the amount of money on saving, " << endl;
+    cin >> afford;
+    // begin if statement check the savings integer are less than $199,999 and greater than 0
+    // nested conditional print message to user 'Keep saving!'
+    if (afford <= 199999 && afford > 0) 
     {
-        cout<<"Keep saving!"<<endl;
+        cout << "Keep saving!" << endl;
     }
-    else if (afford>=200000 && afford<=500000)
+    // else if to check if the savings are between $200,000 and $500,000
+    // nested condition(price range),to, nested conditonal print message(apartment or co-op type)
+    else if (afford >= 200000 && afford <= 500000) 
     {
-        cout<<"Apartment or co-op"<<endl;
-            if(afford>=200000 && afford<=300000)
-                cout<<" :Studio"<<endl;
-            else if(afford>=300001 && afford<=400000)
-                cout<<" :1 BR + 1 Bath"<<endl;
-            else if(afford>=400001 && afford<=500000)
-                cout<<" :2 BRs + 1Bath"<<endl;
+        cout << "With $" << afford << " you can afford an ";
+        cout << "Apartment or co-op";
+        if (afford >= 200000 && afford <= 300000)
+        {
+            cout << " :Studio" << endl;
+        }
+        else if (afford >= 300001 && afford <= 400000)
+        {
+            cout << " :1 BR + 1 Bath" << endl;
+        }
+        else if (afford >= 400001 && afford <= 500000)
+        {
+            cout << " :2 BRs + 1Bath" << endl;
+        }
     }
-    else if (afford>=500001 && afford<=1000000)
+    // nested condition(price range),to, nested conditonal print message(house type)
+    else if (afford >= 500001 && afford <= 1000000) 
     {
-        cout<<"House"<<endl;
-            if(afford>=500001 && afford<=700000)
-                cout<<" :2 BRs + 2 Baths"<<endl;  
-            else if(afford>=700001 && afford<=1000000)
-                cout<<" :3 BRs + 3 Baths"<<endl;
+        cout << "With $" << afford << " you can afford a ";
+        cout << "House";
+        if (afford >= 500001 && afford <= 700000)
+        {
+            cout << " :2 BRs + 2 Baths" << endl;
+        }
+        else if (afford >= 700001 && afford <= 1000000)
+        {
+            cout << " :3 BRs + 3 Baths" << endl;
+        }
     }
-    // check if num3 is the greatest
-    else if (afford>=1000001)
+    // check if the savings are above $1,000,000
+    // conditional print message(mansion type)
+    else if (afford >= 1000001) 
     {
-        cout<<"Mansion"<<endl;
+        cout << "With $" << afford << " you can afford a ";
+        cout << "Mansion" << endl;
     }
-    else (afford<0)
+    // handle invalid or zero/negative savings
+    else 
     {
-        cout<<"Have some savings!"<<endl;
+        cout << "Have some savings!" << endl;
+    }
+
+    // Ask the user to type a number and evolve it if they want to double it
+    double number;
+    char afterselection;
+    cout << "MESSAGE TO USER: Type a number: ";
+    cin >> number;
+    cout << "Does the user want the number to be doubled? (  N/n   or   Y/y  ): ";
+    cin >> afterselection;
+
+    // use switch statement to decide if user wants to double a number
+    // assign switch keyword to char variable afterselection
+    switch (afterselection)
+    {
+        // user input cases
+        // if user inputs N or n, the number remains the same
+    case 'N': case 'n':
+        cout << "The number is set to---------" << number << endl;
+        break;
+    // if user inputs Y or y, the number is doubled using self-multiplication assignment operator
+    case 'Y': case 'y':
+        number *= 2;
+        cout << "The number is set to---------" << number << endl;
+        break;
+    default:
+    // if any other input, set number to 0
+        number = 0;
+        cout << "The number is set to---------" << number << endl;
+        break;
     }
    
     return 0;
-}
+} 
