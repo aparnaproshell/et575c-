d767703cb401bc283040ffad78ccd2dfca393674
aparnaproshell <aparna.harry87@student.qcc.cuny.edu>
2025-10-09 23:32:54 +0000

    finish lab8 completed with for loop mechanism exercise

--- lab8_forloop/lab8_Harry.cpp
+++ lab8_forloop/lab8_Harry.cpp
@@ -63,21 +63,61 @@ int main(){
     cout<<"The total sum of negative numbers= "<<sum_negative<<endl;
 
 
-    cout<<"\n----- EXERCISE 1: ---- "<<endl;
-    // 
-    int f=21;
-    for(f; f>=-21; f--){
-            cout<<f<<"\t";
+
+
+    cout<<"\n----- EXERCISE 1: for loop as a decrement counter with step of 5 ---- "<<endl;
+    // demonstrate a loop body code decrement counter of 21 to -21 (inclusive) in steps of 5
+    // then count in iteration these numbers if they are a multiple of 2, an even number count
+    // declare integer variable to store the value counter f before evaluating its property f%2==0
+    int count_multiples2 = 0; 
+    // start loop with for statement using initial action f = 21
+    // boolean expression as f comparison operator greater than or equal to -21
+    // update action minus assignment operator(self-subtraction) by 5 for each iteration
+    for(int f = 21; f >= -21; f -= 5){
+        // print the current value of f from its passing value incrementing step 5 through for loop
+        // escape character to print horizontal tab space between each value of f
+        cout<<f<<"\t"; 
+        // check if the current value of f is a multiple of 2 (even number) with modulus arithmetic operator %
+        // the modulo operator (%) returns division remainder of 0 as true if f is evenly divisible by 2
+        // therefore using if statement if equal to ==0, f is a multiple of 2, the value counter is multiplied by 2 at each iteration
+        if(f % 2 == 0){
+            // increment operator introduces counter upon this statement if f is even remains true
+            count_multiples2++; 
+        }
     }
+    // blank line for clear output of loop mechanism
+    cout<<endl; 
+    // print in statement the count of times the counter (f value) was a multiple of 2 (even number)
+    cout<<"There is/are "<<count_multiples2<<" even number/s in this sequence."<<endl;
+
+
+    cout<<"\n----- EXERCISE 2: for loop with user input and increment of 4 ---- "<<endl;
+    // demonstrate a loop body code for an input from user, increments by 4 each time in iteration
+    // values printed up to 30 (inclusive)
+    // declare integer variable and store the user's initialization value for the counter
+    int user_input = 0; 
+    // prompt user to enter an initial value from the keyboard
+    cout<<"Enter an initial value: ";
+    // store the console input into variable of for manipulation, user_input
+    cin>>user_input;
+    // input validation: if the user's input is less than or equal to 30, then proceed with this for statement
+    if(user_input <= 30){
+        // the loop starts at value of input as integer variable counter for initial action
+        // boolean expression uses comparison operator less than or equal to 30 increments by 4 each iteration
+        // update action assignment operator (self-addition) to add 4 to the counter upon each iteration
+        for(int counter = user_input; counter <= 30; counter += 4){
+            // print the current value of the counter with each passing value incrementing by step 4
+            // escape character prints horizontal tab space between each value of counter
+            cout<<counter<<"\t"<<endl; 
+        }
+    }
+    else {
+        // necessary is an error condition
+        // handle error if user enters a value greater than 30,the loop will not iterate and print message
+        cout<<"Initial value is greater than 30. Try number 30 or less."<<endl;
+    }
+    // blank line for clear output of loop mechanism
     cout<<endl;
 
-    cout<<"\n----- EXERCISE 2: ---- "<<endl;
-    int user_input = 0;
-    cout<<"Enter an initial value ";
-    cin>>user_input;
-    for(int user_input; user_input<=30; user_input +=4){
-            cout<<user_input<<endl;
-    }
- 
     return 0;
 }
