03f490583b474915554808da5164a05e43c84240
aparnaproshell <aparna.harry87@student.qcc.cuny.edu>
2025-10-17 20:05:09 +0000

    completed lab 9 submitted to brightspace on oct 16, but on github was updated to wrong file. re-update to lab9 for correct examples and exercises

--- lab9_whileloop/lab9_Harry.cpp
+++ lab9_whileloop/lab9_Harry.cpp
@@ -123,7 +123,7 @@ int main(){
     cout<<"Thank you for banking with us!"<<endl;
 
     cout<<"\n ---- example 10: continue statement ----"<<endl;
-    // while loop to print, from 0 to 10, and add numbers all the numbers except 5
+    // while loop to print, from 0 to 10, and add all the numbers except 5
     int sum_all= 0;
     int x = 0;
     while(x++ < 10){        
@@ -138,25 +138,41 @@ int main(){
 
 
     cout<<"\n ---- EXERCISE----"<<endl;
-    int ops = 0;
-    int increase = 0;
+    // declare and initialize integer variables set to zero for all possible integer number inputs
+    // variable to store user input numbers for processing after user's entry from keyboard
+    int ops = 0;        
+    // accumulator variable is stored while running the total of all entered numbers
+    int increase = 0;   
+    // begin do-while loop - execute at least once before checking user input if equal to 0
+    // guarantees the code block in the loop checks condition and runs upon evaluation when true
     do{
+        // ask user to enter a number from the console
         cout<<"Enter a number ";
-        cin>>ops;
+        // after archiving user input and store into ops variable to entail value in the loop operations
+        // maintains the value of each console input every iteration of the loop, and digits the value, dignifies
+        // the added digits to the running total as conditioned by the loop until termination condition is met
+        cin>>ops;   
+        //if user entered 0, sentinel value for termination, the if condition is written with the comparison operator
+        // equal to, to check if the value of ops is equal to 0
         if(ops==0){
-            break;
+            // use break statement to exit the loop immediately when == 0
+            break;  
         }
+        // for any non-zero number, the else block is the if-else statement is executed
         else{
-            increase += ops;
-            cout<<"The total sum is___"<<increase<<endl;
+            // compound assignment operator to add the input number to the running total of the loop
+            // increase(total value) + ops(number value)equals variable increase, self-addition += is used
+            increase += ops;    
+            // print the current running total to user output console after each iteration of the loop
+            // with final setting of total value after accumulating all processes of acquiring a number,
+            // adding numbers in a total, and displaying the current total, variable increase remains with total
+            cout<<"The total sum is____"<<increase<<endl;
         }
     }
+    // while loop: prevail a condition that without terminating the code, there is no exit of the loop iteration
+    // therefore creates an infinite loop which relies on a break statement for termination
+    // the true variable is a boolean literal that is always true, as the loop continues indefinitely until break
     while(true);
     
-    cout<<"The total sum is___"<<increase<<endl;
-
     return 0;
 }
-
-
-
